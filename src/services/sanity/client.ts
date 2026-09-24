f7c917b4a858fecad3bd1a09e31ae172f418bf80
radizjoster-web <radizdirganta0101@gmail.com>
2026-09-24 21:32:55 +0700

    fix: memperbaiki key sanity

--- src/services/sanity/client.ts
+++ src/services/sanity/client.ts
@@ -1,8 +1,8 @@
-import { createClient } from '@sanity/client'
+import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
-  apiVersion: import.meta.env.VITE_SANITY_API_VERSION || '2024-01-01',
+  projectId: import.meta.env.SANITY_PROJECT_ID,
+  dataset: import.meta.env.SANITY_DATASET || 'production',
+  apiVersion: import.meta.env.SANITY_API_VERSION || '2024-01-01',
   useCdn: true,
-})
+});
